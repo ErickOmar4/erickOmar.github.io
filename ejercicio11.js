b9b0dc320711d51f87a0dbea01bed0f77c5edd7f
ErickOmar4 <eroksom47@gmail.com>
2026-07-05 11:27:16 -0600

    Add files via upload

---
+++ ejercicio11.js
@@ -0,0 +1,17 @@
+function convertirKilometros_millas() {
+    const kilometros = document.getElementById('kilometrosIn').value;
+    const soloNumeros = /^\d+$/.test(kilometros);
+    if (kilometros === "") {
+        
+        alert(" ingrese la cantidad de kilometros");
+        /*return;*/
+        
+    } else if(!soloNumeros){
+            alert("ingrese solo números.");
+            return;
+    }else {
+    /*const fahrenheitResult = (celsius * 9/5) + 32;*/
+    const resultadoM =kilometros * 0.621371 ;
+    document.getElementById('resultado_mill').value = resultadoM + " °F";
+    }
+}
